2ba1a9ae83310050da7bea08f069a8f1369f2178
anujkumar.singh <anujkumar.singh@moveinsync.com>
2024-07-20 13:11:43 +0530

    changes

--- Set_1_patterns/pattern1.cpp
+++ Set_1_patterns/pattern1.cpp
@@ -7,9 +7,9 @@ int main()
     cin >> size;
     for (int i = 0; i < size; i++)
     {
-        for (int j = 0; j <= size; j++)
+        for (int j = 0; j < size; j++)
         {
-            cout << "*";
+            cout << "* ";
         }
         cout << "\n";
     }
