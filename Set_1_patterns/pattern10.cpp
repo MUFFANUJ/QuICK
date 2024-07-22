01dd98a255bfb779277001fa8868a39cd565a660
anujkumar.singh <anujkumar.singh@moveinsync.com>
2024-07-22 07:38:02 +0530

    alt 10

--- Set_1_patterns/pattern10.cpp
+++ Set_1_patterns/pattern10.cpp
@@ -23,3 +23,25 @@ int main()
     }
     return 0;
 }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int size;
+//     cin >> size;
+//     int start;
+//     for (int i = 1; i <= 2*size-1; i++)
+//     {
+//       start = i;
+//       if(start > size ) start = 2*size -i;
+//       for (int j = 1; j <= start; j++)
+//       {
+//           cout << "*";
+//       }
+//       cout << "\n";
+//     }
+
+//     return 0;
+// }
