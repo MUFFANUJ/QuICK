c18e329fbc2d949c8cddfcb0330502be0eb38199
anujkumar.singh <anujkumar.singh@moveinsync.com>
2024-07-22 06:47:18 +0530

    pattern 7

---
+++ Set_1_patterns/pattern7.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size;
+    cin >> size;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 1; j <= size - i - 1; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= 2 * i + 1; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
