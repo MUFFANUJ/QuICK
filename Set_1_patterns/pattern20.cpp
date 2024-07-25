1744b2ad2c36f439a95b1f34754cd519f9001c08
anujkumar.singh <anujkumar.singh@moveinsync.com>
2024-07-25 11:46:30 +0530

    alter

--- Set_1_patterns/pattern20.cpp
+++ Set_1_patterns/pattern20.cpp
@@ -38,24 +38,3 @@ int main()
 }
 
 
-
-// #include <iostream>
-// using namespace std;
-
-// int main()
-// {
-//     int size;
-//     cin >> size;
-//     for (int i = 0; i < size; i++)
-//     {
-//       //spaces
-//         for (int j = 0; j < size; j++)
-//         {
-//           if(i == 0 || j == 0 || i == size-1|| j == size-1) cout << "*";
-//           else cout << " ";
-//         }
-        
-//         cout << "\n";
-//     }
-//     return 0;
-// }
