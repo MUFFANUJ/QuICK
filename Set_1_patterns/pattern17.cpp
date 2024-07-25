ac17fab7c861046cd486586930613feac889dedf
anujkumar.singh <anujkumar.singh@moveinsync.com>
2024-07-25 11:44:58 +0530

    better

--- Set_1_patterns/pattern17.cpp
+++ Set_1_patterns/pattern17.cpp
@@ -23,3 +23,34 @@ int main()
     
     return 0;
 }
+
+
+
+//alter better
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int size;
+//     cin >> size;
+//     for (int i = 0; i < size; i++)
+//     {
+//       //spaces
+//         for (int j = 1; j <= size - i - 1; j++)
+//         {
+//             cout << " ";
+//         }
+//         //chars
+//         char ch ='A';
+//         int breakpoint = (2*i)/2 +1;
+//         for (int j = 1; j <= 2 * i + 1; j++)
+//         {
+//             cout << ch;
+//             if(j < breakpoint) ch++;
+//             else ch--;
+//         }
+//         cout << "\n";
+//     }
+//     return 0;
+// }
