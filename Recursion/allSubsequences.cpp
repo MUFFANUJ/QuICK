2fce1bde3d1ddca3899856f3c17903b172abb428
anujkumar.singh <anujkumar.singh@moveinsync.com>
2024-08-10 03:19:06 +0530

    subsequences

---
+++ Recursion/allSubsequences.cpp
@@ -0,0 +1,37 @@
+// using btake or not take
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+void action(int index,vector<int> arr, vector<int> &ds, int size){
+  if (index == size)
+  {
+    for(auto it : ds){
+      cout << it << " ";
+    }
+    cout << "\n";
+    return;
+  }
+  
+  ds.push_back(arr[index]);
+  action(index+1,arr,ds,size);
+  ds.pop_back();
+  action(index+1,arr,ds,size);
+}
+
+
+int main() 
+{
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    for(int i = 0; i < n; ++i) {
+        cin >> vec[i];  
+    }
+    
+    vector<int> ds;
+    action(0,vec,ds,n);
+    return 0;
+}
+
