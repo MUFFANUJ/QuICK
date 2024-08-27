0f5ee827d4cc62bfb938ace630d9ca88ba087555
anujkumar.singh <anujkumar.singh@moveinsync.com>
2024-08-27 11:40:33 +0530

    changes

--- Hashing/integerHashing.py
+++ Hashing/integerHashing.py
@@ -1,7 +1,7 @@
 n = int(input())
 arr = list(map(int,input().split()))
 np = int(input())
-countArr = [0]*(maxSize) # max Size is the the max number that is allowed to be stored in the array provided.
+countArr = [0]*(maxSize) # max Size is the the max number that is allowed to be stored in the array provided. in Python it can be max 10^8 only in cpp inside the int main we can have 10^6 and at global declaration we an have int array space as 10^7.
 for i in range(n):
   countArr[arr[i]] += 1
   
