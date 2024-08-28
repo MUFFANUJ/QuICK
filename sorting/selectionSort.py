729b007069fe341063374bbc07bdd863e9bda2da
anujkumar.singh <anujkumar.singh@moveinsync.com>
2024-08-29 01:02:33 +0530

    ..

--- sorting/selectionSort.py
+++ sorting/selectionSort.py
@@ -11,3 +11,5 @@ for i in range(len(arr)-1):
 print(*arr)
 
 # in selection sort what we do is we take one element from start and find the minimum from the rest of the array and then interchange the values interchange with minimum value with the current value 
+
+# time complexity here is (n*(n+1))/2 which is nothing but O(n^2)
