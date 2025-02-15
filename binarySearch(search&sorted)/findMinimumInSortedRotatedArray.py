2df24f9ec5fb3c7a71a1d930da2a48d87945e53c
anujkumar.singh <anujsinghhero292@gmail.com>
2025-02-15 19:06:15 +0530

    find min in sorted rotated array

---
+++ binarySearch(search&sorted)/findMinimumInSortedRotatedArray.py
@@ -0,0 +1,28 @@
+#here we are finding out the sorted part then storing the minimum in an varible later find the sorted part again and comapring 
+#for the minimum and at last we have the smallest element once the algorithm runs out of options 
+
+# now we can have one more otimization in this is we can add a check for checking if we have the whole array sorted 
+# for ex - arr[low] < arr[high] will tell us if whole array is sorted or not if we find the whole array is sorted
+# there is no need to perfofm further binary search we can directly compare and return the minimum 
+
+def lowerSearch(arr, n ):
+    low = 0
+    high = n-1
+    ans = float('inf')
+    while(low<=high):
+        mid = (low+high)//2
+        if arr[low] <= arr[high]:
+            ans = min(ans,arr[low])
+            break
+        if arr[low] <= arr[mid]:
+            ans = min(ans,arr[low])
+            low = mid + 1
+        else:
+            ans = min(ans,arr[mid])
+            high = mid - 1
+            
+    return ans
+
+    
+arr = list(map(int,input("Enter the array: ").split()))
+print(lowerSearch(arr,len(arr)))
